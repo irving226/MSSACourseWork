9042ebfc3f61192523ee4b8b8a71924137044ebc
Brian Irving <brianirv@buffalo.edu>
2021-03-28 16:34:54 -0600

    Finished Logic for Tic Tac Toe Game

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -26,164 +26,177 @@ namespace TicTacToe
         //thinking of another method that will check if there is a win condition present
         static bool Winner(char[,] gameBoard)
         {
-            int count = 0;
-            char focus;
-            for (int i = 0; i < 3; i++)
+
+
+            //Player X Win Conditions
+            //row 0 win condition
+            if (gameBoard[0, 0] == 'X' && gameBoard[0, 1] == 'X' && gameBoard[0, 2] == 'X')
+            {
+                return true;
+            }
+            //row 1 win condition
+            if (gameBoard[1, 0] == 'X' && gameBoard[1, 1] == 'X' && gameBoard[1, 2] == 'X')
             {
 
-                for (int j = 0; j < 3; j++)
-                {
-                    if (gameBoard[i, j] != '-')
-                    {
-                        count++;
-                        focus = gameBoard[i, j];
+                return true;
+                //row 2 win condition
+            }
+            if (gameBoard[2, 0] == 'X' && gameBoard[2, 1] == 'X' && gameBoard[2, 2] == 'X')
+            {
+                return true;
+            }
 
-                        if (gameBoard[++i, j] == focus)
-                        {
-                            count++;
-                            if (gameBoard[++i, j] == focus)
-                            {
-                                return true;
-                            }
-                        }
-                        else if (gameBoard[i, ++j] == focus)
-                        {
-                            count++;
-                            if (gameBoard[i, ++j] == focus)
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                }
+            //col 0 win condition 
+            if (gameBoard[0, 0] == 'X' && gameBoard[1, 0] == 'X' && gameBoard[2, 0] == 'X')
+            {
+                return true;
+            }
+            //Col 1 win condition
+            if (gameBoard[0, 1] == 'X' && gameBoard[1, 1] == 'X' && gameBoard[2, 1] == 'X')
+            {
+                return true;
+
+                //Col 2 win condition
+            }
+            if (gameBoard[0, 2] == 'X' && gameBoard[1, 2] == 'X' && gameBoard[2, 2] == 'X')
+            {
+                return true;
+            }
+
+            //Diagonal Win Coinditions 
+            if (gameBoard[0, 0] == 'X' && gameBoard[1, 1] == 'X' && gameBoard[2, 2] == 'X')
+            {
+                return true;
+            }
+            if (gameBoard[0, 2] == 'X' && gameBoard[1, 1] == 'X' && gameBoard[2, 0] == 'X')
+            {
+                return true;
+            }
 
 
+            //Player O Win Conditions 
+
+            //row 0 win condition
+            if (gameBoard[0, 0] == 'O' && gameBoard[0, 1] == 'O' && gameBoard[0, 2] == 'O')
+            {
+                return true;
+            }
+            //row 1 win condition
+            if (gameBoard[1, 0] == 'O' && gameBoard[1, 1] == 'O' && gameBoard[1, 2] == 'O')
+            {
+                return true;
+
+            } //row 2 win condition
+            if (gameBoard[2, 0] == 'O' && gameBoard[2, 1] == 'O' && gameBoard[2, 2] == 'O')
+            {
+                return true;
+            }
+            // col 0 win condition
+            if (gameBoard[0, 0] == 'O' && gameBoard[1, 0] == 'O' && gameBoard[2, 0] == 'O')
+            {
+                return true;
+            }
+            //Col 1 win condition
+            if (gameBoard[0, 1] == 'O' && gameBoard[1, 1] == 'O' && gameBoard[2, 1] == 'O')
+            {
+                return true;
+
+            } //Col 2 win condition
+            if (gameBoard[0, 2] == 'O' && gameBoard[1, 2] == 'O' && gameBoard[2, 2] == 'O')
+            {
+                return true;
+            }
+
+            //Diagonal Win Coinditions 
+            if (gameBoard[0, 0] == 'O' && gameBoard[1, 1] == 'O' && gameBoard[2, 2] == 'O')
+            {
+                return true;
+            }
+            if (gameBoard[0, 2] == 'O' && gameBoard[1, 1] == 'O' && gameBoard[2, 0] == 'O')
+            {
+                return true;
             }
             return false;
-
         }
 
-        static bool IsBoardFull(char[,] gameBoard)
-        {
-            for(int i = 0; i < 3; i++)
-            {
-                for(int j = 0; j < 3; j++)
-                {
-                    if (gameBoard[i, j] == '-')
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
+
         static void GameBoard()
         {
-            bool correctVals = false;
-            bool boardIsFull = false;
+
+            bool isLegal = false;
             //initialize board game
             char[,] gameBoard = new char[,] { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
 
-            BoardProgression(gameBoard);
-            //game logic will loop as long as the board is not full 
-            while (boardIsFull != true)
+
+            for (int i = 0; i < 9; i++)
             {
-                //this will loop so long as the board is not full
-                correctVals = false;
-                while (correctVals != true)
+                isLegal = false;
+                while (!isLegal)
                 {
-                    Console.WriteLine("Player 1, please choose a space");
-                    Console.WriteLine("col:");
-                    try
+                    Console.Write("Player one, please choose a row: ");
+                    int row = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter a Col: ");
+                    int col = int.Parse(Console.ReadLine());
+                    if (gameBoard[row, col] != '-')
                     {
-                        int col = int.Parse(Console.ReadLine());
-                        try
+                        isLegal = false;
+                        continue;
+                    }
+                    else
+                    {
+                        gameBoard[row, col] = 'X';
+                        isLegal = true;
+                        BoardProgression(gameBoard);
+                        if (Winner(gameBoard))
                         {
-                            Console.WriteLine("row:");
-                            int row = int.Parse(Console.ReadLine());
-                            if (gameBoard[row, col] != '-')
-                            {
-                                Console.WriteLine("That space has already been chosen, please try again.");
-                                continue;
-                            }
-                            else
-                            {
-                                gameBoard[row, col] = 'X';
-                                BoardProgression(gameBoard);
-                                correctVals = true;
-                                if (Winner(gameBoard))
-                                {
-                                    boardIsFull = true;
-                                    break;
-                                }
-                            }
-                        }
-                        catch (FormatException e)
-                        {
-
+                            Console.WriteLine("Player One is the Winner!");
+                            break;
                         }
                     }
-                    catch (FormatException e)
-                    {
 
-                    }
                 }
-
-                //check to see if board is full before allowing other player to go
-                if (IsBoardFull(gameBoard))
-                {
-                    Console.WriteLine("There are no empty spots on the board! The game ends in a draw.");
-                    boardIsFull = true;
-                };
-
-                correctVals = false;
-                while (correctVals != true && boardIsFull != true)
-                {
-                    Console.WriteLine("Player 2, please choose a space");
-                    Console.WriteLine("col:");
-
-                    try
+               
+                    isLegal = false;
+                    while (!isLegal && !Winner(gameBoard))
                     {
+                        Console.Write("Player two, please choose a row: ");
+                        int row = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter a Col: ");
                         int col = int.Parse(Console.ReadLine());
-                        try
+                        if (gameBoard[row, col] != '-')
                         {
-                            Console.WriteLine("row:");
-                            int row = int.Parse(Console.ReadLine());
-
-                            if (gameBoard[row, col] != '-')
-                            {
-                                Console.WriteLine("That space has already been chosen, please try again.");
-                                continue;
-                            }
-                            else
-                            {
-                                gameBoard[row, col] = 'O';
-                                BoardProgression(gameBoard);
-                                correctVals = true;
-                                if (Winner(gameBoard))
-                                {
-                                    boardIsFull = true;
-
-                                }
-                            }
+                            isLegal = false;
+                            continue;
                         }
-                        catch (FormatException e)
+                        else
                         {
+                            gameBoard[row, col] = 'O';
+                            isLegal = true;
+                            BoardProgression(gameBoard);
+                            if (Winner(gameBoard))
+                            {
+                                Console.WriteLine("Player Two is the Winner!");
+                            }
 
                         }
-                    }
-                    catch (FormatException e)
-                    {
 
                     }
-                }//end of p2 while loop
+                
 
-                //check if board is full
-                if (IsBoardFull(gameBoard)){
-                    Console.WriteLine("There are no empty spots on the board! The game ends in a draw.");
-                    boardIsFull = true;
-                };
-            }//end of game logic while loop
+                if (Winner(gameBoard))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("There are no more open spaces on the board, The game ends in a draw.");
         }
     }
 }
+
+
+
+
+
+
+
+
